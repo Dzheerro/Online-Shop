010fb04794bb366d6cb9daebaf2f38ea042f8cf7
System Administrator <root@MacBook-Pro-Mark.local>
2024-01-05 19:49:18 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'products', component: ProductsComponent},
+    {path: 'backet', component: BasketComponent},
+    {path: 'product/:id', component: ProductDetailsComponent},
+    
+    {path: '**' , redirectTo: '', component: HomeComponent}
+];
